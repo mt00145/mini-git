2d19449c7a99ddfc793a0c5b4699d20f2c3aa31d
mt00145 <mabutareki00145@gmail.com>
2026-05-21 14:16:21 -0400

    Implemented write-tree command for recursively storing directory structure as tree objects

--- ugit/cli.py
+++ ugit/cli.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 
+from . import base
 from . import data
 
 def main():
@@ -38,3 +39,6 @@ def hash_object(args):
 def cat_file(args):
     sys.stdout.flush()
     sys.stdout.buffer.write(data.get_object(args.object, expected=None))
+
+def write_tree (args):
+    print (base.write_tree ())
