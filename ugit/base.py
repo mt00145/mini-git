2876b402d648e1fc4bc9bcbf2a29fe9256a5891a
mt00145 <mabutareki00145@gmail.com>
2026-05-23 15:15:30 -0400

    Add tag support and named refs with OID resolution

--- ugit/base.py
+++ ugit/base.py
@@ -78,7 +78,7 @@ def read_tree(tree_oid):
 def commit(message):
     commit = f'tree {write_tree()}\n'
     
-    HEAD = data.get_HEAD()
+    HEAD = data.get_ref('HEAD')
     if HEAD:
         commit += f'parent {HEAD}\n'
     
@@ -87,14 +87,17 @@ def commit(message):
 
     oid = data.hash_object(commit.encode(), 'commit')
 
-    data.set_HEAD(oid)
+    data.update_ref('HEAD', oid)
 
     return oid
 
 def checkout(oid):
     commit = get_commit(oid)
     read_tree(commit.tree)
-    data.set_HEAD(oid)
+    data.update_ref('HEAD', oid)
+
+def create_tag(name, oid):
+    data.update_ref(f'refs/tags/{name}', oid)
 
 Commit = namedtuple('Commit', ['tree', 'parent', 'message'])
 
@@ -114,6 +117,9 @@ def get_commit(oid):
 
     message = '\n'.join(lines)
     return Commit(tree=tree, parent=parent, message=message)
+
+def get_oid(name):
+    return data.get_ref(name) or name
 
 def is_ignored(path):
     return '.ugit' in path.split('/')
